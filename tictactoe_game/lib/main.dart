80033f6e6b79d4358bd3e7e56d01c2a24727a476
SaadARazzaq <sabdurrazzaq124@gmail.com>
2023-07-17 23:30:56 +0500

    Final game

---
+++ tictactoe_game/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:tictactoe_game/pages/game.dart';
+import 'package:tictactoe_game/pages/home.dart';
+
+
+void main() {
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+      '/': (context) => Home(),
+      '/game': (context) => Game(),
+    },
+    debugShowCheckedModeBanner: false,
+  ));
+}
---
+++ tictactoe_game/lib/pages/home.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  static const Duration fadeInDuration = Duration(seconds: 2);
+  static const Duration fadeOutDuration = Duration(seconds: 4);
+  static const Duration delayDuration = Duration(seconds: 6);
+
+  bool _isVisible = false;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration.zero, () {
+      setState(() {
+        _isVisible = true;
+      });
+    });
+    Future.delayed(delayDuration, () {
+      setState(() {
+        _isVisible = false;
+        Navigator.pushNamed(context, '/game');
+      });
+    });
+  }
+
+  static var customFontWhite = GoogleFonts.bangers(
+    textStyle: TextStyle(
+      color: Colors.white,
+      letterSpacing: 5,
+      fontSize: 36,
+    ),
+  );
+  static var customFontPurple = GoogleFonts.poppins(
+    textStyle: TextStyle(
+      color: Colors.purpleAccent,
+      letterSpacing: 3,
+      fontSize: 18,
+    ),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFBEE5D3),
+      body: AnimatedOpacity(
+        opacity: _isVisible ? 1.0 : 0.0,
+        duration: _isVisible ? fadeInDuration : fadeOutDuration,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "TIC-TAC-TOE",
+                    style: customFontWhite,
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              alignment: Alignment.bottomCenter,
+              child: Text(
+                "By FastDev",
+                style: customFontPurple,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
